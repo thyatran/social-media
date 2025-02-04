bb4b44752281e5e74b7180deeca5e3191f0e4a73
Thy Tran <50391889+thyatran@users.noreply.github.com>
2025-02-04 01:37:10 -0800

    Fix error

--- backend/server.js
+++ backend/server.js
@@ -22,12 +22,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 
-app.use(express.static(path.join(__dirname, "frontend", "dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
-
 app.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server Running on Port ${PORT}`);
