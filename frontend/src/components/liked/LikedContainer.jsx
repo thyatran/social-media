d92d225fdbc9f3685aa1972f424a087b5bdb04f3
Thy Tran <50391889+thyatran@users.noreply.github.com>
2025-02-03 16:25:19 -0800

    Small text changes

--- frontend/src/components/liked/LikedContainer.jsx
+++ frontend/src/components/liked/LikedContainer.jsx
@@ -4,7 +4,7 @@ const LikedContainer = () => {
   return (
     <div className="flex flex-col md:flex-row justify-center items-center w-full min-h-full">
       <div className="flex flex-col border border-gray-900 bg-white w-[400px] h-[600px] rounded-lg shadow-lg">
-        <div className="flex-1 overflow-y-auto p-4">Liked</div>
+        <div className="flex-1 overflow-y-auto p-4">In Progress</div>
       </div>
     </div>
   );
