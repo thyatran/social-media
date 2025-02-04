cc6e48707e356f9f0f939e1e5ba73847b99866d3
Thy Tran <50391889+thyatran@users.noreply.github.com>
2025-02-04 03:16:27 -0800

    Resolve CORS error

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./route/auth.routes.js";
 import userRoutes from "./route/user.routes.js";
@@ -14,6 +15,11 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "https://social-media-fawn-iota.vercel.app",
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
