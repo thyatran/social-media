62ff66898c006bc4811a79c54fd132d44394f6c1
Thy Tran <50391889+thyatran@users.noreply.github.com>
2025-02-06 03:54:50 -0800

    Fix sameSite

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,7 +8,7 @@ const generateTokenAndSetCookie = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: "strict",
+    sameSite: "none",
     secure: process.env.NODE_ENV === "production",
   });
 };
