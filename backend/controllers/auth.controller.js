907d7f57a7fa8f4abd6a6e57d20b027e32bbd29f
Thy Tran <50391889+thyatran@users.noreply.github.com>
2025-02-06 19:03:49 -0800

    Fix sign up

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -29,21 +29,17 @@ export const signup = async (req, res) => {
       bio: bio || "",
     });
 
-    if (newUser) {
-      await newUser.save(); // save user to db
-      // Generate JWT token here
-      generateTokenAndSetCookie(newUser._id, res);
+    await newUser.save(); // save user to db
+    // Generate JWT token here
+    generateTokenAndSetCookie(newUser._id, res);
 
-      res.status(201).json({
-        _id: newUser._id,
-        username: newUser.username,
-        fullname: newUser.fullname,
-        profilePic: newUser.profilePic,
-        bio: newUser.bio,
-      });
-    } else {
-      res.status(400).json({ error: "Invalid user data" });
-    }
+    res.status(201).json({
+      _id: newUser._id,
+      username: newUser.username,
+      fullname: newUser.fullname,
+      profilePic: newUser.profilePic,
+      bio: newUser.bio,
+    });
   } catch (error) {
     console.log("Error in signup controller", error.message);
     res.status(500).json({ error: "Internal Server Error" });
